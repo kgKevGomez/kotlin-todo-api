997981ba75fe517ecbb698c7222967c0712c2ce1
Kevin Gomez <kgomez@phunware.com>
2017-06-13 10:34:49 -0600

    Build process

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -5,8 +5,6 @@
 import spark.Spark.*
 import com.fasterxml.jackson.module.kotlin.*
 fun main(args: Array<String>) {
-    port(8080)
-
     val taskDao = TaskDao()
     path("/api/v1.0") {
         path("/tasks") {
