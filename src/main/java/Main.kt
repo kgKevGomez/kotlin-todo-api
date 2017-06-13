94ae2c00803265f763a5bc1ab7f402bf4de79b07
Kevin Gomez <kgomez@phunware.com>
2017-06-13 12:21:54 -0600

    Build process

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -5,23 +5,35 @@
 import spark.Spark.*
 import com.fasterxml.jackson.module.kotlin.*
 import java.lang.Integer.parseInt
+import spark.Spark.exception
+
+
 
 fun main(args: Array<String>) {
     port(parseInt(System.getenv("PORT")))
 
-    notFound { req, res ->
+    notFound { _, res ->
         res.type("application/json")
         """{"message":"Custom 404"}"""
+    }
+
+    internalServerError { _, res ->
+        res.type("application/json")
+        "{\"message\":\"Custom 500 handling\"}"
+    }
+
+    exception(IllegalArgumentException::class.java) { _, _, response ->
+        response.status(404)
     }
 
     val taskDao = TaskDao()
     path("/api/v1.0") {
         before("/*") {
-            request, response -> response.header("Content-Type", "application/json")
+            _, response -> response.header("Content-Type", "application/json")
         }
         path("/tasks") {
             get("") {
-                req, res ->
+                _, _ ->
                 jacksonObjectMapper()
                     .writeValueAsString(taskDao.tasks)
             }
